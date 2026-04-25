f34d3a01a2f565d27d6d849562c1730851966152
Keshav Kishan <keshavkishan007@gmail.com>
2026-04-26 03:37:48 +0530

    Day 1: added venv setup, gitignore, env structure, dotenv test

---
+++ hello.py
@@ -0,0 +1,7 @@
+# My first Python file
+# The # symbol means this line is a comment -python ignores it
+
+name = "AI Developer" #store a value in a variable
+print("Hello," + name) #print the value of the variable to the console
+
+print("Day 1 complete.") #second line of output
